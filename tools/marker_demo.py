b0d73808fe23fdb19a8f2ead9922a9ea62f3f4a9
JimZ <362765477@qq.com>
2016-10-02 14:39:56 +0800

    update test image directory

--- tools/marker_demo.py
+++ tools/marker_demo.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
         _, _= im_detect(net, im)
 
     # get directory of imagenet
-    test_dir = "/home/shupeng/deepLearning/marker_detection/data/demo/indoor/"
+    test_dir = "./data/demo/indoor/"
     im_names = os.listdir(test_dir)
     for i in xrange(len(im_names)):
         im_names[i] = test_dir + im_names[i]
